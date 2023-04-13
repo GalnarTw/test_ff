3e330cf7c00c46c317fcd0f1b02952e8d776fb43
Nikolai Kolmakov <n.kolmakov@agima.ru>
2023-04-13 19:04:07 +0300

    test

--- lib/uscases/Anime_uscas.dart
+++ lib/uscases/Anime_uscas.dart
@@ -1,9 +1,9 @@
-import 'package:dartz/dartz.dart';
-import 'package:test_ff/http_gg.dart';
-import '/repositories/reosit.dart';
-import '../model/models.dart';
-import '/error/failyre.dart';
-import '/uscases/uscas.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:test_ff/http_gg.dart';
+// import '/repositories/reosit.dart';
+// import '../model/models.dart';
+// import '/error/failyre.dart';
+// import '/uscases/uscas.dart';
 
 // class GetAnime extends Usecase<List<Anime>> {
 //   // final getUrl = GetUrl();
